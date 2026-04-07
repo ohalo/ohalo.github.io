947eccf3b179616c0e040d2e402af1deff8a958b
halo <halo@users.noreply.github.com>
2026-04-07 17:56:51 +0800

    feat: add Google Analytics tracking (G-738KDCJ1C9)

---
+++ javascripts/google-analytics.js
@@ -0,0 +1,12 @@
+// Google Analytics
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-738KDCJ1C9';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-738KDCJ1C9');
+})();
